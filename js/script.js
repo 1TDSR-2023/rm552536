39e0c97985d66bce120602261da2f1a5a96d776c
CavichioliFIAP <rm551100@fiap.com.br>
2023-04-28 23:01:32 -0300

    4.28

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+var nome1 = "Petroni";
+//let nome2 = "Derick";
+//const nome3 = "Ruan";
+
+// regra 1 = inicializar const e não modificar
+const PI = 3.14;
+
+//const e let não sofrem hoisting 
+
+
+if(nome1 !== "" ){
+    let mome1 = "Geovana";
+}
+
+console.log(nome1);
+
