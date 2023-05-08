36cd9183729a15440f2b27e5398265dc9e45f564
Ana <rm552536@fiap.com.br>
2023-05-08 12:47:27 -0300

    ForEach com concatenação. SPREAD( ... )

--- js/script.js
+++ js/script.js
@@ -40,4 +40,71 @@ botao.addEventListener("click", function(){
     this.setAttribute("style", `background-color:rgb(${r},${g},${b});`);
 });
 
+//Declarar um arrary
+//let frutas = ["banana", "maçã", ["pêssego"], ["laranja"], ["limão"], ["melancia"], ["kiwi"]];
+//imprimindo array
+//console.log(frutas);
+//console.log(frutas);
+//console.log(frutas[0]);
+//console.log(frutas[6]);
+
+//Inserir um item ao final do array com método push (NomeDoItem):
+//frutas.push("manga");
+//console.log(frutas);
+//Inserir um item no ínicio do array com método unshift (NomeDoItem): 
+//frutas.unshift("maçã");
+//console.log(frutas);
+
+// //Remover um item ao final do array com o método pop();
+// frutas.pop();
+// console.log(frutas);
+// //Remover um item no início do array com o método shift();
+// frutas.shift();
+// console.log(frutas);
+
+// //Localizar um item no array utilizando o método indexOf(nomeDoItem);
+// //Obs:Este método retorna o índice do item encontrado.
+// // let indice = frutas.indexOf("laranja");
+// // console.log(`O índice encontrado foi : ${indice}`);
+// // console.log(`O item buscado foi : ${frutas[indice]}`);
+
+// //Realizando uma remoção de item utilizando o método splice().
+// //O método splice() recebe dois parâmetros, índice do item que será removido e a quantidade de vezes que este mesmo índice será removido. Ex: splice(indice, 1);
+// //Dica:Utilize o método indexOf() para buscar o índice do item no array.
+// let indice = frutas.indexOf("melancia");
+// console.log(`O item encontrado no índice antes da remoção foi : ${ frutas[indice]}`);
+// frutas.splice(indice,1);
+// console.log(frutas);
+// console.log(`O item encontrado no índice depois da remoção foi : ${ frutas[indice]}`);
+
+// //Concatenação de Arrays
+// let nr1 = [1,2,3,4,5];
+// let nr2 = [6,7,8,9,10];
+// console.log("Array 1 : " + nr1);
+// console.log("Array 2 : " + nr2);
+
+// //Concatenando os dois arrays em um novo array.
+// let nr3 = [nr1,nr2];
+// console.log("Array 3 : " + nr3);
+
+// nr3.forEach( (nr)=>{
+//     nr.forEach((n)=>{
+//         console.log("Itens do Array 3 : " + n);
+//     });
+// } );
+//Concatenação de Arrays
+let nr1 = [1,2,3,4,5];
+let nr2 = [6,7,8,9,10];
+console.log("Array 1 : " + nr1);
+console.log("Array 2 : " + nr2);
+
+//Concatenando os dois arrays em um novo array com o operador SPREAD( ... )
+let nr3 = [...nr1,...nr2];
+console.log("Array 3 : " + nr3);
+
+nr3.forEach( (nr)=>{
+        console.log(`Itens do Array 3 : ${nr}`);
+} );
+
+
 
