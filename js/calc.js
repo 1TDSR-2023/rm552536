8ef76dda6d65869e8e0880a585d347242a8c7f2b
Ana <rm552536@fiap.com.br>
2023-05-17 12:16:35 -0300

    Estilizando a calculadora.

--- js/calc.js
+++ js/calc.js
@@ -1,17 +1,25 @@
 //eval = converte string em expressao matematica
+// arrow function tem estrtura declarativa =>
+// => usada para ser rapida e eh declarativa 
+// function (normal) envolve objetificação e se usa quando tem objeto
 
 function insereNumeroNoVisor(botao, visor){
     visor.value += botao.value
 }
 
-// arrow function tem estrtura declarativa =>
-// => usada para ser rapida e eh declarativa 
-// function (normal) envolve objetificação e se usa quando tem objeto
-
 const calculoDoVisor = (visor)=>{
     visor.value = eval(visor.value);
 }
 
+function insereNumeroNoVisor(botao, visor){
+    if(botao.value == "+" || botao.value == "-" || botao.value == "*" || botao.value == "/"){
 
-
-// desafio 3 impeca a entrada de mais de uma vez do operador matematico
+    if (visor.value[visor.value.lenght-1] == "+" || visor.value[visor.value.lenght-1] == "-" || visor.value[visor.value.lenght-1] == "*" || visor.value[visor.value.lenght-1] == "+/" ){
+        visor.value.slice(visor.value.lenght-1)
+     }else{
+        visor.value += botao.value
+    }
+        }else{
+            visor.value += botao.value
+    }
+}
